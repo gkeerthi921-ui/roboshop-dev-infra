23d9a08afc248ed23a136724636070874ab348b3
gkeerthi921-ui <gkeerthi921@gmail.com>
2025-11-20 23:07:01 +0530

    terraform

--- 40-database/bootstrap.sh
+++ 40-database/bootstrap.sh
@@ -1,4 +1,4 @@
  #!/bin/bash
-#  dnf install ansible -y
-#  #
-#  ansible -pull -u https://github.com/gkeerthi921-ui/ansible.git
+  dnf install ansible -y
+  
+  ansible-pull -u https://github.com/gkeerthi921-ui/ansible.git -e component=mongodb main.yaml
