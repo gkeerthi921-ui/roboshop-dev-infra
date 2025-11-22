769c1f66c32f9af40a213dc31d9d2a7866beedaf
gkeerthi921-ui <gkeerthi921@gmail.com>
2025-11-22 12:33:30 +0530

    terraform

--- 40-database/dat.tf
+++ 40-database/dat.tf
@@ -1,5 +1,6 @@
 data "aws_ssm_parameter""mongodb_sg_id" {
-     name  ="/${var.project_name}/${var.environment}/bastion_sg_id"
+    # name  ="/${var.project_name}/${var.environment}/bastion_sg_id"
+      name  ="/${var.project_name}/${var.environment}/mongodb_sg_id"
 }
 
 data "aws_ssm_parameter" "redis_sg_id" {
@@ -15,7 +16,8 @@ data "aws_ssm_parameter" "rabbitmq_sg_id" {
 # }     
 
 data "aws_ssm_parameter""database_subnet_ids" {
-     name  ="/${var.project_name}/${var.environment}/bastion_sg_id"
+    # name  ="/${var.project_name}/${var.environment}/bastion_sg_id"
+      name  ="/${var.project_name}/${var.environment}/database_subnet_ids"
 }
 data "aws_ami" "joindevops" {
     owners           = ["973714476881"]
