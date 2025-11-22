9c7b99a2273828e2f628e3b888b02dfda0d54981
gkeerthi921-ui <gkeerthi921@gmail.com>
2025-11-22 13:00:13 +0530

    terraform

--- 40-database/dat.tf
+++ 40-database/dat.tf
@@ -11,9 +11,9 @@ data "aws_ssm_parameter" "rabbitmq_sg_id" {
   name = "/${var.project_name}/${var.environment}/rabbitmq_sg_id"
 }
 
-# data "aws_ssm_parameter" "mysql_sg_id" {
-#   name = "/${var.project_name}/${var.environment}/mysql_sg_id"
-# }     
+data "aws_ssm_parameter" "mysql_sg_id" {
+  name = "/${var.project_name}/${var.environment}/mysql_sg_id"
+}     
 
 data "aws_ssm_parameter""database_subnet_ids" {
     # name  ="/${var.project_name}/${var.environment}/bastion_sg_id"
